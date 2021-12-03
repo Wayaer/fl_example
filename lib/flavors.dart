ea64d7cfef64ff6cd56e26567a0898e18fbace84
Wayaer <wayaer@foxmail.com>
2021-12-03 11:03:44 +0800

    add flavors

---
+++ lib/flavors.dart
@@ -0,0 +1,26 @@
+enum Flavor {
+  APPLE,
+  BANANA,
+}
+
+extension FlavorName on Flavor {
+  String get name => this.toString().split('.').last;
+}
+
+class F {
+  static Flavor? appFlavor;
+
+  static String get name => appFlavor?.name ?? '';
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.APPLE:
+        return 'Apple App';
+      case Flavor.BANANA:
+        return 'Banana App';
+      default:
+        return 'title';
+    }
+  }
+
+}
