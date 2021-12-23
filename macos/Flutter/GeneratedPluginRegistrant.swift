169e4769d90eb2f30c02371239b7adf31826df99
Wayaer <wayaer@foxmail.com>
2021-12-23 17:42:44 +0800

    no message

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_curiosity
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CuriosityPlugin.register(with: registry.registrar(forPlugin: "CuriosityPlugin"))
 }
